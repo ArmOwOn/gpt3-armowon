1ae01e515d255ff4306d3a168bc23a759c0c442c
Armaan Seydi <armaanseydi@gmail.com>
2024-04-21 13:10:45 +0330

    setting up components

---
+++ src/components/cta/Cta.tsx
@@ -0,0 +1,6 @@
+import "./cta.css";
+const Cta = () => {
+  return <div>Cta</div>;
+};
+
+export default Cta;
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Article from "./article/article";
+import Brand from "./brand/brand";
+import Cta from "./cta/Cta";
+import Feature from "./feature/Feature";
+import Navbar from "./navbar/Navbar";
+
+export { Article, Brand, Cta, Feature, Navbar };
